8551311b8872b07fa87c491caf9ff0e651fbd376
conradorpg <conrado.romancene@saudeid.com.br>
2022-04-19 18:07:08 -0300

    Criação cadastro Produtos

--- cypress/integration/3-meus-testes/register.spec.js
+++ cypress/integration/3-meus-testes/register.spec.js
@@ -8,13 +8,17 @@ describe('Realizar registro do usuário', () => {
   
   
   it('Registrar usuário com sucesso', () => {
-    cy.get('#reg_email').type(faker.internet.email())
+    let nome = faker.name.firstName()
+    let sobrenome = faker.name.lastName()
+    let email = faker.internet.email(nome)
+
+    cy.get('#reg_email').type(email)
     cy.get('#reg_password').type('test@123')
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(faker.name.firstName())
-    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('#account_first_name').type(nome)
+    cy.get('#account_last_name').type(sobrenome)
 	  // cy.get('#account_display_name').type('TestCy')
 	  // cy.get('#account_email').type('test+8@mail.com')
 	  cy.get('.woocommerce-Button').click()
